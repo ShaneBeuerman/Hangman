580bf91391133200ee407818c1bc312131c0e913
ShaneBeuerman <shanebeuerman@gmail.com>
2021-05-27 13:17:47 -0600

    Update GUI
    GUI changes based on the string you submit with the entry field. If your string is longer than one character, an error pops up.

--- Hangman.py
+++ Hangman.py
@@ -1,21 +1,39 @@
 #Hangman
 import tkinter
 
-def GUI():
+def updateGUI(letter):
     global blanks
-    Game = tkinter.Tk()
-    blank = tkinter.Label(text=blanks)
-    prompt = tkinter.Label(text="What letter would you like to pick?")
-    guess = tkinter.Entry()
-    submit = tkinter.Button(text="Submit")
-    blank.pack()
-    prompt.pack()
-    guess.pack()
-    submit.pack()
-    Game.mainloop()
+    global blank
+    global test
+    global strikes
+    if letter in test:
+        for i in range(len(test)):
+            if test[i] == letter:
+                blanks[i] = letter
+        blank.config(text=blanks)
+    else:
+        strikes += 1
+        print("You have", strikes, "strikes")
+        
+def errorPopup():
+    error = tkinter.Tk()
+    error.wm_title("Error")
+    errorLabel = tkinter.Label(error, text="Sorry, just one character please.")
+    errorButton = tkinter.Button(error, text="Okay", command = error.destroy)
+    errorLabel.pack()
+    errorButton.pack()
+    error.mainloop()
 
-
-
+def submit():
+    global guess   
+    letter = str(guess.get())
+    if len(letter) > 1:
+        print("Sorry, just one character please")
+        errorPopup()
+        return
+    updateGUI(letter)
+    print(letter)
+    
 test = "Hello World"
 strikes = 0
 blanks = []
@@ -26,9 +44,22 @@ for i in test:
         blanks.append("_")
 
 print(blanks)
-GUI()
+Game = tkinter.Tk()
+blank = tkinter.Label(text=blanks)
+prompt = tkinter.Label(text="What letter would you like to pick?")
+guess = tkinter.Entry()
+submit = tkinter.Button(text="Submit", command=submit)
+gallows = tkinter.Canvas(Game, width=100, height=100)
+blank.pack()
+prompt.pack()
+guess.pack()
+submit.pack()
+gallows.pack()
+Game.mainloop()
 while strikes < 5 and "_" in blanks:
     letter = input("What letter would you like to pick?")
+    if letter == 'quit':
+        break
     if letter not in test:
         strikes += 1
         print("You have", strikes)
